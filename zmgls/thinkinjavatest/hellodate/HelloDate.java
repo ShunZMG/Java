6915c7b3d8efb42f249092ee0f5d1d07a2b50dca
ShunZMG <1397779430@qq.com>
2019-06-14 00:34:52 +0800

    Initial commit

---
+++ zmgls/thinkinjavatest/hellodate/HelloDate.java
@@ -0,0 +1,10 @@
+package com.zmgls.thinkinjavatest.hellodate;
+
+import java.util.*;
+
+public class HelloDate{
+    public static void main(String[] args) {
+        System.out.println("Hello, it's:");
+        System.out.println(new Date());
+    }
+}
---
+++ zmgls/thinkinjavatest/quotetest/QuoteTest.java
@@ -0,0 +1,29 @@
+package com.zmgls.thinkinjavatest.quotetest;
+/**
+ * 引用测试
+ * t2 = t1;
+ * t1、t2均为引用，将t1赋值给t2，即将t2改为对t1所引用的对象的引用
+ */
+public class QuoteTest{
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        Test t2 = new Test();
+
+        t1.set("abc");
+        System.out.println(t1.get());
+        t2 = t1;
+        t2.set("diie");
+        System.out.println(t1.get());
+    }
+}
+class Test{
+    private String teststr;
+        
+    public String get(){
+        return teststr;
+    }
+    
+    public void set(String str){
+        this.teststr = str;
+    }
+}
